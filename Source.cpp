04b8e9244a6ff231fd356144d4dd9c6a906447ad
Валерия Овсейчук <55581836+Lerok2112@users.noreply.github.com>
2019-11-27 20:00:44 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n = 9, count = 0;
+	string *mas = new string[n];
+	char numbers[10];
+	cout << "Input string:" << endl;
+	cin >> numbers;
+	cout << "Input array of strings:" << endl;
+	for (int i = 0; i < n; i++) {
+		cin >> mas[i];
+		if (numbers[i] == '1') count++;
+	}
+	string *new_mas = new string[count];
+	for (int i = 0, j = 0; i < n; i++) {
+		if (numbers[i] == '1') {
+			new_mas[j] = mas[i];
+			j++;
+	}
+	}
+	for (int i = 0; i < count; i++) {
+		cout << new_mas[i] << " ";
+	}
+	delete[] mas;
+	delete[] new_mas;
+	return 0;
+}
