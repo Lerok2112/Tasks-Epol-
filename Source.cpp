503393d645fb5cee849c1bd8f0a82cf73ae81fd2
Валерия Овсейчук <55581836+Lerok2112@users.noreply.github.com>
2019-11-27 20:11:55 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -12,17 +12,20 @@ int main() {
 		cin >> mas[i];
 		if (numbers[i] == '1') count++;
 	}
-	string *new_mas = new string[count];
-	for (int i = 0, j = 0; i < n; i++) {
-		if (numbers[i] == '1') {
-			new_mas[j] = mas[i];
-			j++;
+	if (count != 0) {
+		string *new_mas = new string[count];
+		for (int i = 0, j = 0; i < n; i++) {
+			if (numbers[i] == '1') {
+				new_mas[j] = mas[i];
+				j++;
+			}
+		}
+		for (int i = 0; i < count; i++) {
+			cout << new_mas[i] << " ";
+		}
+		delete[] mas;
+		delete[] new_mas;
 	}
-	}
-	for (int i = 0; i < count; i++) {
-		cout << new_mas[i] << " ";
-	}
-	delete[] mas;
-	delete[] new_mas;
+	else cout << "String input error!" << endl;
 	return 0;
 }
